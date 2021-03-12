3010e924c31a259f7b515ce0caad1a525cf1651b
Febri Ardi Saputra <ardisaputrafebri@gmail.com>
2021-03-12 14:10:57 +0700

    Fixing News Detail

--- pages/news/_slug.vue
+++ pages/news/_slug.vue
@@ -2,8 +2,8 @@
     .news-detail.mt-3(v-if="news")
       .hide-on-mobile
         .news-title
-          h3.mb-0 {{news.title}}
-          p.text-size-mini.font-weight-light.mt-1.mb-2 Dibuat Pada: {{news.created_at | formatDate('DD-MM-YYYY HH:mm')}}
+          h2.mb-0 {{news.title}}
+          p.text-size-mini.font-weight-light.mt-1.mb-2 {{news.created_at | formatDate('dddd, DD-MM-YYYY HH:mm')}}
         .news-image.text-center
           img.w-100.img-detail(:src="news.image")
         .news-detail
@@ -14,7 +14,7 @@
             .col-md-5.col-sm-5
                 img.w-100.img-detail(:src="news.image")
             .col
-                h3.mb-0 {{news.title}}
+                h2.mb-0 {{news.title}}
                 .news-detail
                     p.text-size-mini.font-weight-light.mt-1.mb-2 Dibuat Pada: {{news.created_at | formatDate('DD-MM-YYYY HH:mm')}}
                     .news-content(v-html="news.content")
